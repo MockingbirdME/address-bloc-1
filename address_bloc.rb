dbbcf8efa03ba144e993ac212ad652b627e913d1
Devon Henegar <devon.henegar@gmail.com>
2017-01-24 14:32:59 -0500

    checkpoint 3 complete

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,4 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
 require_relative 'controllers/menu_controller'
+require 'bloc_record'
+
+ BlocRecord.connect_to("db/address_bloc.sqlite")
+
 
 menu = MenuController.new
 system "clear"
