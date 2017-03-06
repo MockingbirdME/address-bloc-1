e769ca1a16252bb193eb4968c12709348bbb6752
Devon Henegar <devon.henegar@gmail.com>
2017-03-06 07:59:31 -0500

    made checkpoint 7 adjustment, per assignment

--- address_bloc.rb
+++ address_bloc.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require_relative 'controllers/menu_controller'
 require 'bloc_record'
 
- BlocRecord.connect_to("db/address_bloc.sqlite")
+ BlocRecord.connect_to("db/address_bloc.db", dbType)
 
 
 menu = MenuController.new
